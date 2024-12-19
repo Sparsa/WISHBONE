769258e0da04e6f52095c5d6604390f3b061b259
Sparsa Roychowdhury <sparsa.roychowdhury@tu-causthal.de>
2024-12-19 15:08:08 +0100

    made necessary changes in the files

--- clash/src/Control.hs
+++ clash/src/Control.hs
@@ -31,31 +31,30 @@ data Input domain a =
   Input
     { dat_i :: Signal domain a
     , cyc_i :: Signal domain Bool
-    , rst_i :: Signal domain Bool
     , stb_i :: Signal domain Bool
     }
 
 -----------------------------------------------------------------------------
 
-data Output domain  =
+data Output domain b =
   Output
     { ack_o :: Signal domain Bool
-    , odd :: Signal domain Bool
+    , odd :: Signal domain b
     }
 
 -----------------------------------------------------------------------------
 
-data Functions a =
+data Functions a b =
   Functions
-    { checkOdd :: a -> Bool
+    { checkOdd :: a -> b
     }
 
 -----------------------------------------------------------------------------
 
-data InitialState  =
+data InitialState b =
   InitialState
     { ack_o :: Bool
-    , odd :: Bool
+    , odd :: b
     }
 
 -----------------------------------------------------------------------------
@@ -64,7 +63,6 @@ data ControlIn domain =
   ControlIn
     { controlIn0 :: Signal domain Bit
     , controlIn1 :: Signal domain Bit
-    , controlIn2 :: Signal domain Bit
     }
 
 -----------------------------------------------------------------------------
@@ -75,33 +73,31 @@ data ControlOut domain =
     , controlOut1 :: Signal domain Bit
     , controlOut2 :: Signal domain Bit
     , controlOut3 :: Signal domain Bit
-    , controlOut4 :: Signal domain Bit
     }
 
 -----------------------------------------------------------------------------
 
 control
-  :: HiddenClockResetEnable domain 
-  => Functions a
-  -> InitialState 
+  :: HiddenClockResetEnable domain
+  => ( NFDataX b)
+  => Functions a b
+  -> InitialState b
   -> Input domain a
-  -> Output domain 
+  -> Output domain b
 
 control Functions{..} InitialState{..} Input{..} =
   let
     ack_oCell = register ack_o ack_oOut
     oddCell = register odd oddOut
 
-    w3 = pure True
-    w4 = pure False
-    w8 = checkOdd <$> dat_i
+    w3 = checkOdd <$> dat_i
+    w4 = pure True
 
     ControlOut{..} =
       controlCircuit
         ControlIn
           { controlIn0 = ((! 0) . pack) <$> cyc_i
           , controlIn1 = ((! 0) . pack) <$> stb_i
-          , controlIn2 = ((! 0) . pack) <$> rst_i
           }
 
     ack_oOut =
@@ -109,18 +105,16 @@ control Functions{..} InitialState{..} Input{..} =
         ( liftA2 (++#) (fmap pack controlOut0)
         $ fmap pack controlOut1
         )
-        ( liftA2 (:>) w3
+        ( liftA2 (:>) w4
         $ liftA2 (:>) ack_oCell
         $ pure Nil
         )
     oddOut =
       oddSwitch
         ( liftA2 (++#) (fmap pack controlOut2)
-        $ liftA2 (++#) (fmap pack controlOut3)
-        $ fmap pack controlOut4
+        $ fmap pack controlOut3
         )
-        ( liftA2 (:>) w8
-        $ liftA2 (:>) w4
+        ( liftA2 (:>) w3
         $ liftA2 (:>) oddCell
         $ pure Nil
         )
@@ -133,7 +127,7 @@ control Functions{..} InitialState{..} Input{..} =
 -----------------------------------------------------------------------------
 
 ack_oSwitch
-  :: HiddenClockResetEnable domain 
+  :: HiddenClockResetEnable domain
   => Signal domain (BitVector 2)
   -> Signal domain (Vec 2 a)
   -> Signal domain a
@@ -148,36 +142,28 @@ ack_oSwitch = liftA2 select
 
 oddSwitch
   :: HiddenClockResetEnable domain 
-  => Signal domain (BitVector 3)
-  -> Signal domain (Vec 3 a)
+  => Signal domain (BitVector 2)
+  -> Signal domain (Vec 2 a)
   -> Signal domain a
 
 oddSwitch = liftA2 select
   where
     select bs vs
-      | bs ! 0 == high = vs !! 2
-      | bs ! 1 == high = vs !! 1
+      | bs ! 0 == high = vs !! 1
       | otherwise      = vs !! 0
 
 -----------------------------------------------------------------------------
 
 controlCircuit
- :: HiddenClockResetEnable domain 
+ :: HiddenClockResetEnable domain
  => ControlIn domain -> ControlOut domain
 
 controlCircuit ControlIn{..} = 
-  let
-    o2 = _not_ controlIn2
-  in
     ControlOut
       { controlOut0 = pure high
       , controlOut1 = pure low
-      , controlOut2 = o2
-      , controlOut3 = controlIn2
-      , controlOut4 = pure low
+      , controlOut2 = pure high
+      , controlOut3 = pure low
       }
-
-  where
-    _not_ = fmap complement
 
 -----------------------------------------------------------------------------
--- clash/src/TopEntity.hs
+++ clash/src/TopEntity.hs
@@ -35,8 +35,8 @@ import qualified Control as Co (Output (..))
     , t_output =
         PortProduct ""
           [ 
-           PortName "ACK"
-           PortName "ODD"
+           PortName "ACKv"
+          ,PortName "ODD"
           ]
     }
   )
@@ -47,23 +47,8 @@ import qualified Control as Co (Output (..))
 --type Digit = Unsigned 7
 
 -----------------------------------------------------------------------------
-type Width = Unsigned 64
--- data DAT_I = DAT_I :: 
------------------------------------------------------------------------------
---data Time = Time
---  { secD0 :: Unsigned 4
---  , secD1 :: Unsigned 4
---  , minD0 :: Unsigned 4
---  , minD1 :: Unsigned 4
---  , ticks :: Unsigned 19
---  } deriving (Eq, Generic, NFDataX)
-
------------------------------------------------------------------------------
-
---zero :: Time
---zero = Time 0 0 0 0 0
-
------------------------------------------------------------------------------
+type Width = BitVector 64
+--------------------------------
 
 topEntity
   :: Clock System
@@ -75,171 +60,24 @@ topEntity
 
 topEntity clk rst stb_i cyc_i dat_i  = withClockResetEnable clk rst enableGen $
   let
-    stb_i' = debounce stb_i 
-    cyc_i' = debounce cyc_i 
-    dat_i' = debounse dat_i
+   -- stb_i' = debounce stb_i 
+   -- cyc_i' = debounce cyc_i 
+   --  dat_i' = debounce dat_i
     functions = Functions  TopEntity.checkOdd 
-    input = Input (pure 1) stb_i' cyc_i'  dat_i'
+    input = Input dat_i cyc_i  stb_i
     initialstate = InitialState False False
  
     output = control
       functions
       initialstate
       input
-    bundle (ack_o',odd')
-      where  
-      ack_o' = unbundle (Co.ack_o output)
-      odd' = unbundle (Co.odd output)
+  in bundle ((Co.ack_o output),(Co.odd output))
    -- bundle (ack_o', odd')
    -- (d0, d1, d2, d3) =  unbundle (Co.dsp output )
    -- in
    -- bundle (d0, d1, d2, d3, beeper  (Co.beep output))
-
---{-# NOINLINE topEntity #-}
+{-# NOINLINE topEntity #-}
 -- Here you have to define the functions
 -----------------------------------------------------------------------------
-checkOdd :: Signal System ( Width ) -> Bool
-checkOdd data_i = last Width
---countup :: Time -> Unsigned 19 -> Time
---countup t@Time{..} dt
---  | ticks < 333000 = t { ticks = ticks + dt }
---  | otherwise      = incSec t { ticks = 0 }
---
--------------------------------------------------------------------------------
---countdown :: Time -> Unsigned 19 -> Time
---countdown t@Time{..} dt
---  | ticks >= dt = t { ticks = ticks - dt }
---  | secD0 > 0   = upd t { secD0 = secD0 - 1 }
---  | secD1 > 0   = upd t { secD1 = secD1 - 1, secD0 = 9 }
---  | minD0 > 0   = upd t { minD0 = minD0 - 1, secD1 = 5, secD0 = 9 }
---  | minD1 > 0   = upd t { minD1 = minD1 - 1, minD0 = 9, secD1 = 5, secD0 = 9 }
---  | otherwise   = TopEntity.zero
---  where
---    upd x = x { ticks = 333000 - dt + ticks }
---
--------------------------------------------------------------------------------
---
---incSec :: Time -> Time
---incSec t@Time{..}
---  | secD0 < 9 = t { secD0 = secD0 + 1 }
---  | secD1 < 5 = t { secD1 = secD1 + 1, secD0 = 0 }
---  | minD0 < 9 = t { minD0 = minD0 + 1, secD0 = 0, secD1 = 0 }
---  | minD1 < 9 = t { minD1 = minD1 + 1, minD0 = 0, secD0 = 0, secD1 = 0 }
---  | otherwise = t { minD1 = 0, minD0 = 0, secD0 = 0, secD1 = 0 }
---
--------------------------------------------------------------------------------
---
---incMin :: Time -> Time
---incMin t@Time{..}
---  | minD0 < 9 = t { minD0 = minD0 + 1 }
---  | minD1 < 9 = t { minD1 = minD1 + 1, minD0 = 0 }
---  | otherwise = t { minD1 = 0, minD0 = 0 }
---
--------------------------------------------------------------------------------
---
---display :: Time -> (Digit, Digit, Digit, Digit)
---display Time{..} =
---  ( toDigit minD1
---  , toDigit minD0
---  , toDigit secD1
---  , toDigit secD0
---  )
---
--------------------------------------------------------------------------------
---
---toDigit :: Unsigned 4 -> Digit
---toDigit = \case
---  0 -> 8
---  1 -> 91
---  2 -> 34
---  3 -> 18
---  4 -> 81
---  5 -> 20
---  6 -> 4
---  7 -> 90
---  8 -> 0
---  _ -> 16
---
--------------------------------------------------------------------------------
---
---debounce
---  :: HiddenClockResetEnable domain
---  => Signal domain Bool -> Signal domain Bool
---
---debounce input =
---  let
---    state = register False state'
---    counter = register 0 counter'
---    r = upd <$> input <*> state <*> counter
---    (state',counter') = unbundle r
---  in
---    state'
---  where
---    upd
---      :: Bool -> Bool -> Unsigned 11 -> (Bool, Unsigned 11)
---    upd i s c
---      | c > 0     = (s, c - 1)
---      | i == s    = (s, 0)
---      | otherwise = (i, 1665)
---
--------------------------------------------------------------------------------
---
---beeper
---  :: HiddenClockResetEnable domain
---  => Signal domain Bool
---  -> Signal domain Bool
---
---beeper enabled =
---  let
---    hold = register False hold'
---    counter = register (0 :: Unsigned 19) counter'
---
---    hold' =
---      (hold `_and_` ((< 40000) <$> counter))
---      `_or_` (enabled `_and_` ((< 320000) <$> counter))
---
---    counter' = gate id hold' (+1) (const 0) counter
---  in
---    gate id (hold' `_and_` (beeptime <$> counter))
---#ifdef PASSIVEBUZZER
---      id (const False) beepFreq
---#else
---      id (const False) (pure True)
---#endif
---  where
---    beeptime x =
---        x <  19000
---      || x >=  40000 && x <  59000
---      || x >=  80000 && x <  99000
---      || x >= 120000 && x < 139000
---    _or_ = liftA2 (||)
---    _and_ = liftA2 (&&)
---
--------------------------------------------------------------------------------
---
---#ifdef PASSIVEBUZZER
---beepFreq
---  :: HiddenClockReset domain gated synchronous
---  => Signal domain Bool
---
---beepFreq =
---  let
---    output = register False output'
---    step = register (0 :: Unsigned 6) step'
---
---    output' = gate (== 0) step not id output
---    step' = gate (< 36) step (+ 1) (const 0) step
---  in
---    output'
---#endif
---
--------------------------------------------------------------------------------
---
---gate
---  :: Applicative f
---  => (a -> Bool) -> f a -> (b -> c) -> (b -> c) -> f b -> f c
---
---gate check b u1 u2 x =
---  (\a b -> if check a then u1 b else u2 b) <$> b <*> x
---
------------------------------------------------------------------------------
+checkOdd ::  Width  -> Bool
+checkOdd dat_i = (bitToBool . lsb) dat_i
